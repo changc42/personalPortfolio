8b834fe3ff57a18ac7ebb8b2a831ac30b54b33cd
Caleb Chang <changc42@gmail.com>
2020-03-15 22:29:52 -0400

    successfully sent static build index.html on request to unknown route. console logging process.env.NODE_ENV

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(__dirname);
+app.get("/api/", (req, res) => {
+  res.send("home page of express routes");
 });
 
-app.get("/something", (req, res) => {
-  res.send("yo moomm");
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  const path = require("path");
+  console.log(process.env);
+  res.sendFile(path.resolve(__dirname, "client/build/index.html"));
 });
-
-app.use(express.static("public"));
 
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
