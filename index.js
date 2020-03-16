8a6b3c0f887358848d384898bd94a39b8671ff64
Caleb Chang <changc42@gmail.com>
2020-03-15 21:51:05 -0400

    switched order of static serve and '/' get requests

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
   const path = require("path");
+  console.log(__dirname);
   res.sendFile(path.resolve(__dirname, "public/panda2.jpg"));
 });
 
 app.get("/something", (req, res) => {
   res.send("yo moomm");
 });
+
+app.use(express.static("public"));
 
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
